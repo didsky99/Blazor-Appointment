bfe309a822265e693e0770bb67684d71254deb70
Dimas Ariadi <ariadimas1705@gmail.com>
2025-10-29 23:57:11 +0700

    Base project setup

---
+++ Shared/Models/DTOs/EmployeeTimeEvent/EmployeeTimeEventResponse.cs
@@ -0,0 +1,27 @@
+
+namespace Models.Dtos.EmployeeTimeEvent;
+
+public class EmployeeTimeEventResponse
+{
+    public string Nrp { get; set; } = null!;
+
+    public DateTime EventDate { get; set; }
+
+    public TimeSpan? ClockIn { get; set; }
+
+    public string? LatLongIn { get; set; }
+
+    public DateTimeOffset? TimeStampIn { get; set; }
+
+    public TimeSpan? ClockOut { get; set; }
+
+    public string? LatLongOut { get; set; }
+
+    public DateTimeOffset? TimeStampOut { get; set; }
+
+    public string? TimeZone { get; set; }
+
+    public DateTime? UpdateDate { get; set; }
+
+    public string? UpdateBy { get; set; }
+}
